103d875f832bac255f8dde1dde1ec417a2b8e4b1
rishitkundu1 <85060575+rishitkundu1@users.noreply.github.com>
2022-08-15 12:15:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function preload(){}
+function setup(){
+      canvas = createCanvas(650, 450);
+      canvas.center();
+}
+function draw(){}
+function saveImage(){
+      save('myFilterImage.png')
+}
